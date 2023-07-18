ea6a6a13aea3d1c69febe07b0b2129777434cdc9
DESKTOP-HTPDBCF\ROG Zephyrus <dungnc69.420@gmail.com>
2023-07-18 15:49:26 +0700

    Configure

--- src/main/java/com/fullstack/Backend/BackendApplication.java
+++ src/main/java/com/fullstack/Backend/BackendApplication.java
@@ -1,12 +1,8 @@
 package com.fullstack.Backend;
 
-import java.util.concurrent.Executor;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
 @EnableAsync
