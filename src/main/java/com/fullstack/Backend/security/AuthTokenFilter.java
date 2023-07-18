b14f8bf0f4583092f5d8f858d7a4ac4d13e98f73
DESKTOP-HTPDBCF\ROG Zephyrus <dungnc69.420@gmail.com>
2023-07-18 17:00:57 +0700

    Configure

--- src/main/java/com/fullstack/Backend/security/AuthTokenFilter.java
+++ src/main/java/com/fullstack/Backend/security/AuthTokenFilter.java
@@ -1,6 +1,5 @@
 package com.fullstack.Backend.security;
 
-import com.fullstack.Backend.services.impl.UserDetailsServiceImpl;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -22,7 +22,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
     private JwtUtils jwtUtils;
 
     @Autowired
-    private UserDetailsServiceImpl _userDetailsService;
+    private UserDetailsService _userDetailsService;
 
     @Value("${app.client.baseUrl}")
     String baseUrl;
