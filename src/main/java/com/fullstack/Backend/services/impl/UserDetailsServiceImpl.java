354f4f0dcb947ff8819b477f1b0c87c2b9148f83
DESKTOP-HTPDBCF\ROG Zephyrus <dungnc69.420@gmail.com>
2023-07-18 16:08:07 +0700

    Configure

--- src/main/java/com/fullstack/Backend/services/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/fullstack/Backend/services/impl/UserDetailsServiceImpl.java
@@ -9,9 +9,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-
 /* Returns a UserDetails object that Spring Security can use for authentication and validation.
  *  UserDetails contains necessary information (such as: username, password, authorities) to build an Authentication object.
  *
@@ -19,16 +16,13 @@ import java.util.concurrent.ExecutionException;
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
-    UserService _userService;
+    UserRepository _userRepository;
 
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        CompletableFuture<User> user = _userService.findByUsername(username);
-        try {
-            return UserDetailsImpl.build(user.get());
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
+        User user = _userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+        return UserDetailsImpl.build(user);
     }
 }
